5ae0f1c283707af5d37daddf2e46a758f2d702c7
masaya7 <52489219+masaya7@users.noreply.github.com>
2023-06-27 20:10:26 +0900

    fix:ユニーク制約追加

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,6 +2,6 @@ class Relationship < ApplicationRecord
   belongs_to :user
   belongs_to :follower, class_name: 'User'
 
-  validates :user_id, presence: true
-  validates :follower_id, presence: true
+  validates :user_id, presence: true, uniqueness: true
+  validates :follower_id, presence: true, uniqueness: true
 end
