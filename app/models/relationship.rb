c98ec29c3e5961ed268fb38640fc96f2e1db8599
masaya7 <52489219+masaya7@users.noreply.github.com>
2023-06-27 22:09:08 +0900

    fix:複合ユニーク制約追加

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,6 +2,6 @@ class Relationship < ApplicationRecord
   belongs_to :user
   belongs_to :follower, class_name: 'User'
 
-  validates :user_id, presence: true, uniqueness: true
-  validates :follower_id, presence: true, uniqueness: true
+  validates :user_id, presence: true,uniqueness: { scope: :follower_id }
+  validates :follower_id, presence: true,uniqueness: { scope: :user_id }
 end
